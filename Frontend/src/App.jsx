d9027a374a461ebd92eaf43b66e1da5d1c15a9c3
prabhatchauhan04 <prabhat14chauhan2004@gmail.com>
2025-10-02 19:33:44 +0530

    Frontend card updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import socket from './socket/socket.js'; // this line alone connected frontend to backend bcoz abhi import hote time puri file chli thi
 import Editor from '@monaco-editor/react';
 import { nanoid } from 'nanoid';
+import helloWorldVideo from './assets/helloworld.mp4';
 
 
 
@@ -155,46 +156,59 @@ function App() {
         return (
             <>
                 <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4">
-                    <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md space-y-6 border border-gray-200">
-                        <h1 className="text-3xl font-bold text-gray-800 text-center">Join a Code Room</h1>
-
-                        {/* So the input does not maintain its own value anymore. It always “reads” from the roomId state. That’s why this is called a controlled component. */}
-                        <input
-                            type="text"
-                            placeholder="Room ID"
-                            value={roomId} // Whatever is in your React state variable roomId will always be displayed in the input box.
-                            onChange={(e) => setRoomId(e.target.value)} // Whenever you type something, the input’s new text (e.target.value) is stored in your React state (roomId).
-                            // If onChange is not there: The input becomes read-only — you can’t type anything because React state (value={roomId}) controls it.Typing won’t change the state, so the input always shows whatever is in roomId.
-                            className="w-full p-3 rounded-lg bg-gray-100 text-gray-800 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500"
-                        />
-
-                        <input
-                            type="text"
-                            placeholder="Your Name"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            className="w-full p-3 rounded-lg bg-gray-100 text-gray-800 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500"
-                        />
-
-                        <div className="flex justify-between items-center">
-                            <span className="text-sm text-gray-500">Need a room?</span>
-                            <button
-                                onClick={generateRoomId}
-                                className="text-xs bg-blue-100 hover:bg-blue-200 active:bg-blue-300 text-blue-700 font-medium px-3 py-1 rounded-md transition active:scale-95"
+                    <div className='h-3/4 w-1/2 border-gray-200 shadow-2xl rounded-2xl flex flex-row overflow-hidden'>
+                        <div className='w-1/2'>
+                            <video
+                                autoPlay
+                                loop
+                                muted
+                                className="w-full h-full object-cover scale-100 origin-center pointer-events-none select-none"
+                                onContextMenu={(e) => e.preventDefault()}
                             >
-                                Generate Room ID
+                                <source src={helloWorldVideo} type="video/mp4" />
+                            </video>
+                        </div>
+                        <div className="bg-white p-8 shadow-xl w-1/2 max-w-md space-y-6 border border-gray-200">
+                            <h1 className="text-3xl font-bold text-gray-800 text-center">Join a Code Room</h1>
+
+                            {/* So the input does not maintain its own value anymore. It always “reads” from the roomId state. That’s why this is called a controlled component. */}
+                            <input
+                                type="text"
+                                placeholder="Room ID"
+                                value={roomId} // Whatever is in your React state variable roomId will always be displayed in the input box.
+                                onChange={(e) => setRoomId(e.target.value)} // Whenever you type something, the input’s new text (e.target.value) is stored in your React state (roomId).
+                                // If onChange is not there: The input becomes read-only — you can’t type anything because React state (value={roomId}) controls it.Typing won’t change the state, so the input always shows whatever is in roomId.
+                                className="w-full p-3 rounded-lg bg-gray-100 text-gray-800 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500"
+                            />
+
+                            <input
+                                type="text"
+                                placeholder="Your Name"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                className="w-full p-3 rounded-lg bg-gray-100 text-gray-800 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-violet-500"
+                            />
+
+                            <div className="flex justify-between items-center">
+                                <span className="text-sm text-gray-500">Need a room?</span>
+                                <button
+                                    onClick={generateRoomId}
+                                    className="text-xs bg-blue-100 hover:bg-blue-200 active:bg-blue-300 text-blue-700 font-medium px-3 py-1 rounded-md transition active:scale-95"
+                                >
+                                    Generate Room ID
+                                </button>
+
+                            </div>
+
+                            <button
+                                onClick={joinRoom}
+                                className="w-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 text-white font-semibold py-3 rounded-lg shadow-md transition duration-200 active:scale-95"
+                            >
+                                Join Room
                             </button>
 
+
                         </div>
-
-                        <button
-                            onClick={joinRoom}
-                            className="w-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 text-white font-semibold py-3 rounded-lg shadow-md transition duration-200 active:scale-95"
-                        >
-                            Join Room
-                        </button>
-
-
                     </div>
                 </div>
             </>
